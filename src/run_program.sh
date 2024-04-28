65ad046d4fbaa149bb9e82c7827b49afbec1fbd5
Jinesh Patel <jinesh@Jineshs-MacBook-Pro.local>
2024-04-27 20:03:22 -0700

    added script that runs the program

--- src/run_program.sh
+++ src/run_program.sh
@@ -12,3 +12,9 @@ tokens=$(python3 tokenizer.py "$1")
 # Run the parser and save the parsed output
 parsed_output=$(swipl -q -s parser.pl -g "phrase(program(Program), $tokens), writeq(Program), halt." -t "halt.")
 
+# Run the evaluator on the parsed output
+evaluation_result=$(swipl -q -s Evaluator.pl -g "program_eval($parsed_output, []), halt." -t "halt.")
+
+
+# Display the evaluation result
+echo "$evaluation_result"
